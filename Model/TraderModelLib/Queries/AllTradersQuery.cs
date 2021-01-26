1b116870af2fffc957dedc1d6964fe374f9106ed
ladni <ladnik@hotmail.com>
2021-01-26 18:38:27 +0200

    Naive sorting added

--- Model/TraderModelLib/Queries/AllTradersQuery.cs
+++ Model/TraderModelLib/Queries/AllTradersQuery.cs
@@ -12,11 +12,39 @@ namespace TraderModelLib.Queries
         public AllTradersQuery(IRepo<TraderDbContext> repo)
         {
             FieldAsync<ListGraphType<TraderType>>("traders",
-                arguments: new QueryArguments(new QueryArgument<BooleanGraphType> { Name = "isDeleted" }), 
+                arguments: new QueryArguments(
+                        new QueryArgument<BooleanGraphType> { Name = "isDeleted" },
+                        new QueryArgument<StringGraphType> { Name = "sortBy" }
+                    ), 
                 resolve: async context =>
                 {
                     var isDeleted = (bool)context.Arguments["isDeleted"];
+                    var sortBy = (string)context.Arguments["sortBy"];
+
                     var traders = await repo.FetchAsync(dbContext => dbContext.Traders.Where(t => t.IsDeleted == isDeleted).ToList());
+
+                    if (!string.IsNullOrEmpty(sortBy)) 
+                        switch (sortBy.ToLower()) 
+                        {
+                            case "birthdate":
+                            case "age":
+                                traders = traders.OrderBy(t => t.Birthdate).ToList();
+                                break;
+
+                            case "!birthdate":
+                            case "!age":
+                                traders = traders.OrderByDescending(t => t.Birthdate).ToList();
+                                break;
+
+                            case "email":
+                                traders = traders.OrderBy(t => t.Email).ToList();
+                                break;
+
+                            case "!email":
+                                traders = traders.OrderByDescending(t => t.Email).ToList();
+                                break;
+                        }
+
                     context.SetCache<GqlCache>("traderIds", traders.Select(t => t.Id).ToList());
                     return traders;
                 });
@@ -24,10 +52,12 @@ namespace TraderModelLib.Queries
     }
 }
 
+//OrderByDescending
+
 /*
 query AllTraders {
   allTradersQuery {
-    traders(isDeleted: false) {
+    traders(isDeleted: false, sortBy: "age") {
       id
       firstName
       lastName
