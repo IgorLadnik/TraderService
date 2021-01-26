cf10fbb0a00bb7b46139def15db567bde1de9783
ladni <ladnik@hotmail.com>
2021-01-26 16:00:35 +0200

    Mutation fix - 2

--- Model/TraderModelLib/Data/TraderDbContext.cs
+++ Model/TraderModelLib/Data/TraderDbContext.cs
@@ -6,6 +6,8 @@ namespace TraderModelLib.Data
 {
     public class TraderDbContext : DbContext
     {
+        public static int currentId = 100;
+
         public static string ConnectionString { private get; set; }
 
         public TraderDbContext()
@@ -25,23 +27,23 @@ namespace TraderModelLib.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Trader>().HasData(
-                new { Id = 1, FirstName = "Vasya", LastName = "Pupkin", Birthdate = new DateTime(1990, 1, 1),  Email = "vpupkin@trader.com".ToLower(), IsDeleted = false, Password = "vvv" },
-                new { Id = 2, FirstName = "Moshe", LastName = "Levy", Birthdate = new DateTime(1991, 1, 1), Email = "mlevy@trader.com", IsDeleted = false, Password = "mmm" }
+                new { Id = 0, FirstName = "Moshe", LastName = "Cohen", Birthdate = new DateTime(1991, 1, 1), Email = "mcohen@trader.com", IsDeleted = false, Password = "mmm" },
+                new { Id = 1, FirstName = "Vasya", LastName = "Pupkin", Birthdate = new DateTime(1990, 1, 1),  Email = "vpupkin@trader.com".ToLower(), IsDeleted = false, Password = "vvv" }
             );
 
             modelBuilder.Entity<Cryptocurrency>().HasData(
-             new { Id = 1,  Currency = "Bitcoin", Symbol = "XBT" },
-             new { Id = 2, Currency = "Litecoin", Symbol = "LTC" },
-             new { Id = 3, Currency = "Namecoin", Symbol = "NMC" }
+             new { Id = 0, Currency = "Bitcoin",  Symbol = "XBT" },
+             new { Id = 1, Currency = "Litecoin", Symbol = "LTC" },
+             new { Id = 2, Currency = "Namecoin", Symbol = "NMC" }
             );
 
             modelBuilder.Entity<TraderToCurrency>().HasData(
-             new { Id = 1, TraderId = 1, CurrencyId = 1 },
-             new { Id = 2, TraderId = 1, CurrencyId = 2 },
-             new { Id = 3, TraderId = 1, CurrencyId = 3 },
-             new { Id = 4, TraderId = 2, CurrencyId = 1 },
-             new { Id = 5, TraderId = 2, CurrencyId = 2 },
-             new { Id = 6, TraderId = 2, CurrencyId = 3 }
+             new { Id = 0, TraderId = 0, CurrencyId = 0 },
+             new { Id = 1, TraderId = 0, CurrencyId = 1 },
+             new { Id = 2, TraderId = 0, CurrencyId = 2 },
+             new { Id = 3, TraderId = 1, CurrencyId = 0 },
+             new { Id = 4, TraderId = 1, CurrencyId = 1 },
+             new { Id = 5, TraderId = 1, CurrencyId = 2 }
             );
         }
     }
