7e1ad6365f1888548b3028da69c023f544d7fc2e
ladni <ladnik@hotmail.com>
2021-01-27 19:40:45 +0200

    Test with in-mem service

---
+++ Test/TraderServiceTest/GqlControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TraderServiceTest
+{
+    [TestClass]
+    public class GqlControllerTests : IntegrationTest
+    {
+        const string query =
+            @"query Traders {
+              tradersQuery {
+                traders(
+                  isDeleted: false 
+                    sortBy: '!Birthdate'
+    	            pageSize: 2
+    	            currentPage: 0) {
+                    id
+                    firstName
+                    lastName
+                    birthdate
+                    email
+                    password
+                    isDeleted
+                    cryptocurrencies
+                        {
+                            id
+                            currency
+                            symbol
+                        }
+                    }
+                }
+            }";
+
+        const string mutation =
+            @"mutation TraderMutation {
+                traderMutation {
+                    createTraders(
+                      tradersInput: 
+                      [
+                        {
+                            firstName: 'Lior'
+                            lastName: 'Levy'
+                            birthdate: '1950-01-01'
+                            email: 'llevy@trader.com'
+                            password: 'lll'
+                            isDeleted: false
+                            cryptocurrencies: [{ id: 0 }{ id: 1 }]
+   	                    }
+                        {
+                            firstName: 'Ann'
+                            lastName: 'Linders'
+                            birthdate: '1980-01-01'
+                            email: 'annl@trader.com'
+                            password: 'lll'
+                            isDeleted: false
+                            cryptocurrencies: [{ id: 0 }{ id: 0 }]
+   	                    }
+                      ]
+                    ) {
+                        status
+                        message
+                    }
+                }
+            }";
+
+        [TestMethod]
+        public async Task TestQuery()
+        {
+            var result = await Execute(query);
+        }
+
+        [TestMethod]
+        public async Task TestMutation()
+        {
+            var result = await Execute(mutation);
+        }
+    }
+}
---
+++ Test/TraderServiceTest/IntegrationTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using TraderService;
+
+namespace TraderServiceTest
+{
+    public class IntegrationTest
+    {
+        const string _gqlUri = "https://localhost:7001/gql";
+
+        private readonly HttpClient _testClient;
+
+        public IntegrationTest()
+        {
+            // In-memory service
+            _testClient = new WebApplicationFactory<Startup>().CreateClient();
+        }
+
+        protected async Task<string> Execute(string query)
+        {
+            var postData = new { Query = Adjust(query) };
+            StringContent stringContent = new(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+            var response = await _testClient.PostAsync(_gqlUri, stringContent);
+            return response.IsSuccessStatusCode
+                ? await response.Content.ReadAsStringAsync()
+                : $"Error. Status code: {response.StatusCode}";
+        }
+
+        protected static string Adjust(string query) => query?.Replace("'", "\"");
+    }
+}
