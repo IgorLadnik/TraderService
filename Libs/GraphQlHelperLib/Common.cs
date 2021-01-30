a8fee08e761b12dc1c57c283ce585fcfb0b035e0
ladni <ladnik@hotmail.com>
2021-01-30 21:20:42 +0200

    Remove extra stuff

--- Libs/GraphQlHelperLib/Common.cs
+++ Libs/GraphQlHelperLib/Common.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using GraphQL;
 using GraphQL.Execution;
 
